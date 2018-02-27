3d43d9246a24b9cad4fe9b1ec799968cf4977960
mahfuzak08 <mahfuzak08@gmail.com>
2018-02-27 15:57:20 +0600

    Setup app for Heroku and mlab.com for MongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mahfuzak08:ma592321@ds249718.mlab.com:49718/todoapp' || 'mongodb://localhost:27017/TodoApp');
+if(process.env.POST){
+  mongoose.connect('mongodb://mahfuzak08:ma592321@ds249718.mlab.com:49718/todoapp');
+}else{
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 
 module.exports = {mongoose};

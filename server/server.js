29b08153bb183256744902be88be74b8026c7348
mahfuzak08 <mahfuzak08@gmail.com>
2018-02-28 17:19:29 +0600

    POST user/login

--- server/server.js
+++ server/server.js
@@ -119,6 +119,18 @@ app.get('/user/me', authenticate, (req, res) =>{
   res.send(req.user);
 })
 
+app.post('/user/login', (req, res) =>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+  Users.findByCredentials(body.email, body.password).then((user) =>{
+    return user.generateAuthToken().then((token) =>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) =>{
+    res.status(400).send();
+  })
+});
+
 app.listen(port, () =>{
   console.log(`Server is up in ${port} port.`);
 });
